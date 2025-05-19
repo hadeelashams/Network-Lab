b07732fee3d9b2d705d06fe098a62b02709f066b
hadeelashams <124576825+hadeelashams@users.noreply.github.com>
2025-05-19 12:38:28 +0530

    Add files via upload

---
+++ rev.sh
@@ -0,0 +1,20 @@
+#file reverse
+if [ $# -ne 1 ];then
+echo "Syntax is $0 <filename>"
+exit 1
+fi
+set -xv 
+cp $1 file
+l=`cat file | wc -l`
+rm rfile
+while [ $1 -ge 1 ]
+do
+tail -n 1 file >> rfile
+l=`expr $l - 1`
+head -n $l file > temp
+mv temp file 
+done
+
+
+
+
