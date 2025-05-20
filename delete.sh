b6ba68c8fc99af01fdf6fcb2ad93de2796780f6b
hadeelashams <124576825+hadeelashams@users.noreply.github.com>
2025-05-20 15:21:50 +0530

    complete

--- delete.sh
+++ delete.sh
@@ -7,9 +7,9 @@ fi
 cmp -s $1 $2
 if [ $? -eq 0 ]
 then
-echo "Removing file $1"
+echo "Files are identical \nRemoving file $1"
 rm $1
 else
-echo "Files are not identical"
+echo "Files $1 and $2 not identical"
 fi 
 exit 0
